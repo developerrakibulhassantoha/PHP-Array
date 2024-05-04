a84b4941a0958592370e47b157678e613862f42d
rakib <rakibweb68@gmail.com>
2024-05-04 22:19:10 +0600

    file add

--- prerecored video class/array.php
+++ prerecored video class/array.php
@@ -1,23 +1,7 @@
 <?php
-$students = array (
-    "Rakib",
-    "Anika",
-    24,
-    "Sharmin",
 
-);
-//echo count($students);
-//echo $students[1];
-//var_dump($students);
 
-//loop jkn soro take calano hoy
-$n =count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
 
-// loop jkn ses take calano hoy
- $n =count($students);
-for($i=$n-1; $i>=0; $i--) {
-    echo $students[$i]."\n";
-}
+
+
+
